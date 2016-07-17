f35ca4a7f9f328b9ccbec637e47ddde55f978cd3
Wayne's Bioinformatics Code Portal <fomightez@gmail.com>
2016-07-17 16:23:01 -0400

    fix typo in comments for argparse section

--- find_overlap_in_lists_with_Venn.py
+++ find_overlap_in_lists_with_Venn.py
@@ -376,7 +376,7 @@ parser.add_argument("Files", help="List the names of FILES containing the lists 
     to compare. AT LEAST ONE IS REQUIRED. Of course, it makes more sense with \
     two or more.", nargs="+")
 # see http://stackoverflow.com/questions/13219910/argparse-get-undefined-number-of-arguments
-# for hot last line allowd any number of files to be used.
+# for how last line allows any number of files to be used.
 parser.add_argument("-s", "--sensitive",help=
     "add this flag to force comparison of items to be case-sensitive (NOT \
     recommended). Default (recommended) is to make the comparisons independent \
